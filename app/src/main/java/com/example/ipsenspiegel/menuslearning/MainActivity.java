93e3f19abb28853b407db283731c5e67d82eec50
John <serradillamate.alejandro@gmail.com>
2016-11-24 10:24:26 +0100

    Like implemented

--- app/src/main/java/com/example/ipsenspiegel/menuslearning/MainActivity.java
+++ app/src/main/java/com/example/ipsenspiegel/menuslearning/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.ipsenspiegel.menuslearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-
+    TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.hello = (TextView)findViewById(R.id.hello_TxTView);
+        this.registerForContextMenu(this.hello);
+
     }
 
     @Override
@@ -28,10 +36,27 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mMenuInflater = this.getMenuInflater();
         mMenuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mContMenuInflater = this.getMenuInflater();
+        mContMenuInflater.inflate(R.menu.main_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.Like){
+            Log.i("TAG", "El menu secundario ha sido pulsado");
+            return true;
+        }
+        return false;
     }
 }
